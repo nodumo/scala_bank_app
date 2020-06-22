ceeb4034d6ee46f568adf002963481afb3fe87d0
Nick Odumo <odumowebdev@gmail.com>
2020-06-22 11:20:39 -0400

    Minor simplification

--- application/src/main/scala/org/nodumo/bankapp/application/usecases/transaction/interpreters/CreditUsecaseCatsEffectInterpreter.scala
+++ application/src/main/scala/org/nodumo/bankapp/application/usecases/transaction/interpreters/CreditUsecaseCatsEffectInterpreter.scala
@@ -59,6 +59,7 @@ class CreditUsecaseCatsEffectInterpreter(
                                                               TimeStamp,
                                                               DateTime])(implicit monad: Monad[IO])
     extends UseCaseAlgebra[IO, CreditTransactionCommand, CreditTransactionResponse] {
+      
   /* DateTime ->  Previous date (1 day ago) */
   private def previousDay(dateTime: DateTime) =
     dateTimeGetPreviousDaysService.getPreviousDayStart(Days.days(1))(dateTime)
@@ -67,14 +68,14 @@ class CreditUsecaseCatsEffectInterpreter(
   private def previousWeek(dateTime: DateTime) =
     dateTimeGetPreviousWeekService.getPreviousWeeksStart(Weeks.weeks(1))(dateTime)
 
-  /* Raw domain object -> (TransactionError \/ Domain object) */
+  /* Raw domain object -> (TransactionError OR Domain object) */
   private def createCreditTransaction(
       creditTransaction: CreditTransaction.Repr): Either[TransactionError, CreditTransaction] =
     factoryCreditTransactionService
       .create(creditTransaction)
       .bimap(factoryTransactionErrorServiceDomainValueError.create, id)
 
-  /* Raw domain object -> (TransactionError \/ Domain credit transaction) */
+  /* Raw domain object -> (TransactionError  OR Domain credit transaction) */
   private def validateTransaction(
       creditTransaction: CreditTransaction,
       dailyTransactionFrame: List[CreditTransaction],
@@ -83,7 +84,7 @@ class CreditUsecaseCatsEffectInterpreter(
       .validate(creditTransaction, dailyTransactionFrame, weeklyTransactionFrame)
       .bimap(factoryTransactionErrorServiceTransactionError.create, id)
 
-  /* Raw domain object -> (TransactionError \/ Domain credit transaction */
+  /* Raw domain object -> (TransactionError OR  Domain credit transaction */
   override def execute(event: CreditTransactionCommand): IO[CreditTransactionResponse] =
     createCreditTransaction(factoryCreditTransactionReprService.create(event)) match {
       // If left then the input data is garbage
@@ -91,14 +92,17 @@ class CreditUsecaseCatsEffectInterpreter(
       // If right then the application data is valid
       case Right(creditTransaction @ CreditTransaction(_, customerId, _, timeStamp)) =>
         for {
+          
           // Repository: Retrieve of the transactions after 1 day ago
           oneDayTransactionsFrame <- transactionRepository.getCreditTransactionsAfterCertainDate(
             customerId,
             previousDay(timeStamp.value))
+          
           // Repository: Retrieve all of the transactions since 1 week ago
           oneWeekTransactionSample <- transactionRepository.getCreditTransactionsAfterCertainDate(
             customerId,
             previousWeek(timeStamp.value))
+          
           response <- validateTransaction(creditTransaction, oneDayTransactionsFrame, oneWeekTransactionSample) match {
             // If Left(error) then we have encountered a validation error return an error
             case Left(e) =>
@@ -110,6 +114,7 @@ class CreditUsecaseCatsEffectInterpreter(
               transactionRepository.createCreditTransaction(creditTransaction) >> monad.pure(
                 succeedTransaction(event.id, event.customer_id))
           }
+          
         } yield response
     }
 
